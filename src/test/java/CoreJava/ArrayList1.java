f90156b779894de911b8724a29766eca4817bbef
Nikhila9346 <nikhilabeereddy@gmail.com>
2025-12-02 18:24:48 +0530

    arrayList

--- src/test/java/CoreJava/ArrayList1.java
+++ src/test/java/CoreJava/ArrayList1.java
@@ -4,15 +4,24 @@ import java.util.ArrayList;
 
 public class ArrayList1 {
 	public static void main(String args[]) {
-		//ArrayList - variable length
+		//ArrayList - It is a class which implements List Interface, variable length, duplicates are accepted, 
 		ArrayList<String> list = new ArrayList<String>();
 		
-		//add values into ArrayList
+		//add values into ArrayList - 2ways 
 		list.add("Nikhila");
 		list.add("Nishitha");
+		list.add(2, "Nikhil");
 		
 		//get values from the ArrayList
 		System.out.print(list.get(0));
+		
+		//to get the index of a value
+		System.out.println(list.indexOf("Nikhil"));
+		
+		//remove the values
+		list.remove(0);
+		list.remove("Nikhila");
+		list.removeAll(list);
 		
 		//to get size of arraylist
 		System.out.println(list.size());
@@ -20,6 +29,8 @@ public class ArrayList1 {
 		//to check element is present or not
 		System.out.println(list.contains("Nikhila"));
 		
+		//isEmpty()
+		System.out.println(list.isEmpty());
 		
 	}
 }
